0760440d7698db2078aff30ac9783f1b9d38e147
michele <michele.damico@gmail.com>
2017-06-10 16:37:17 +0200

    Removed references to djset in API

--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,16 @@ impl <'a> DjSetContainer<'a> {
         ret
     }
 
-    pub fn find(&self, djset: &DjSet) -> DjSet {
-        if let Some(parent) = self.nodes[*djset].parent {
-            return self.find(&parent);
+    pub fn find(&mut self, djset: DjSet) -> DjSet {
+        match self.nodes[djset].parent {
+            Some(parent) => {
+                self.find(parent)
+            }
+            None => djset.clone()
         }
-        djset.clone()
     }
 
-    pub fn union(&mut self, left: &DjSet, right: &DjSet) {
+    pub fn union(&mut self, left: DjSet, right: DjSet) {
         let left = self.find(left);
         let right = self.find(right);
 
@@ -47,8 +49,6 @@ impl <'a> DjSetContainer<'a> {
     fn merge(&mut self, n0: DjSet, n1: DjSet) {
         self.nodes[n0].parent = Some(n1)
     }
-
-
 }
 
 #[cfg(test)]
@@ -63,7 +63,7 @@ mod tests {
         let a = dj.add(&P);
         let b = dj.add(&P);
 
-        assert!(dj.find(&a) != dj.find(&b));
+        assert!(dj.find(a) != dj.find(b));
     }
 
     #[test]
@@ -71,7 +71,7 @@ mod tests {
         let mut dj = DjSetContainer::new();
         let a = dj.add(&P);
 
-        assert!(dj.find(&a) == dj.find(&a));
+        assert!(dj.find(a) == dj.find(a));
     }
 
     #[test]
@@ -80,8 +80,10 @@ mod tests {
         let a = dj.add(&P);
         let b = dj.add(&P);
 
-        dj.union(&a, &b);
+        dj.union(a, b);
 
-        assert!(dj.find(&a) == dj.find(&b));
+        assert!(dj.find(a) == dj.find(b));
     }
+
+
 }
