d6a0240105d4d779906bdae88a383e88ad9aa398
michele <michele.damico@gmail.com>
2017-06-10 16:27:16 +0200

    Quick and dirty

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 pub type DjSet = usize;
 
 struct Node<'a>{
-    payload: &'a Payload
+    payload: &'a Payload,
+    parent: Option<DjSet>
 }
 
 pub struct DjSetContainer<'a> {
@@ -24,49 +25,63 @@ impl <'a> DjSetContainer<'a> {
 
     pub fn add(&mut self, payload: &'a Payload) -> DjSet {
         let ret = self.nodes.len();
-        let node = Node::<'a> { payload: payload };
+        let node = Node::<'a> { payload: payload, parent: None };
         self.nodes.push(node);
         ret
     }
 
     pub fn find(&self, djset: &DjSet) -> DjSet {
+        if let Some(parent) = self.nodes[*djset].parent {
+            return self.find(&parent);
+        }
         djset.clone()
     }
 
-    pub fn union(&mut self, other: &Self) {
+    pub fn union(&mut self, left: &DjSet, right: &DjSet) {
+        let left = self.find(left);
+        let right = self.find(right);
+
+        self.merge(left, right);
     }
+
+    fn merge(&mut self, n0: DjSet, n1: DjSet) {
+        self.nodes[n0].parent = Some(n1)
+    }
+
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    static P: Payload = 12;
+
     #[test]
     fn two_elements_are_not_equivalent() {
-        let p = 12;
         let mut dj = DjSetContainer::new();
-        let a = dj.add(&p);
-        let b = dj.add(&p);
+        let a = dj.add(&P);
+        let b = dj.add(&P);
 
         assert!(dj.find(&a) != dj.find(&b));
     }
 
     #[test]
     fn one_element_is_equivalent_to_itself() {
-        let p = 1;
         let mut dj = DjSetContainer::new();
-        let a = dj.add(&p);
+        let a = dj.add(&P);
 
         assert!(dj.find(&a) == dj.find(&a));
     }
-//
-//    #[test]
-//    fn after_union_two_sets_should_become_equivalent() {
-//        let a = DjSet::new(12);
-//        let b = DjSet::new(12);
-//
-//        a.union(&b);
-//
-//        assert!(a.find() == b.find());
-//    }
+
+    #[test]
+    fn after_union_two_sets_should_become_equivalent() {
+        let mut dj = DjSetContainer::new();
+        let a = dj.add(&P);
+        let b = dj.add(&P);
+
+        dj.union(&a, &b);
+
+        assert!(dj.find(&a) == dj.find(&b));
+    }
 }
